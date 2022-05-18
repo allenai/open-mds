8b2d0b76c6a8a0565e2430bca66cd62ec1ed3705
John Giorgi <johnmgiorgi@gmail.com>
2022-05-18 14:07:44 -0700

    Add pytest fixture for huggingface datasets

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,25 @@
 from typing import Callable
 import pytest
 from transformers import AutoTokenizer, PreTrainedTokenizer
+import datasets
+
+
+@pytest.fixture
+def hf_dataset() -> Callable:
+    """This is a fixture factory. It returns a function that you can use
+    to create a HuggingFace dataset object. Optional **kwargs are passed to `load_dataset()`.
+    """
+
+    def _hf_dataset(model_name_or_path: str, **kwargs) -> PreTrainedTokenizer:
+        return datasets.load_dataset(model_name_or_path, **kwargs)
+
+    return _hf_dataset
 
 
 @pytest.fixture
 def hf_tokenizer() -> Callable:
     """This is a fixture factory. It returns a function that you can use
-    to create an AllenNLP `Vocabulary` object. It accepts optional `**extras`
-    which will be used along with `params` to create the `Vocabulary` object.
+    to create a HuggingFace tokenizer object. Optional **kwargs are passed to `from_pretrained()`.
     """
 
     def _hf_tokenizer(model_name_or_path: str, **kwargs) -> PreTrainedTokenizer:
