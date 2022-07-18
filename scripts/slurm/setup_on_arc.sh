c4659e91b5e4c16fa9b5edbc7deab76d03a31ee3
John Giorgi <johnmgiorgi@gmail.com>
2022-07-18 14:47:21 -0700

    Update the setup on arc script

--- scripts/slurm/setup_on_arc.sh
+++ scripts/slurm/setup_on_arc.sh
@@ -8,30 +8,36 @@
 # Load the required modules
 # Notes: 
 # - arrow needed for HF Datasets both during installation and use
-# - rust is needed for HF Tokenizers library (and possibly other libraries) only during installation
 # - java needed for Terrier (via PyTerrier) both during installation and use
-module load StdEnv/2020 gcc/9.3.0 python/3.8 cuda/11.4 arrow/7.0.0 rust/1.59.0 java/11.0.2
+module purge
+module load StdEnv/2020 gcc/9.3.0 python/3.8 arrow/7.0.0
+
+# Setup the virtual environment under home
+PROJECT_NAME="retrieval-exploration"
+virtualenv --no-download "$HOME/$PROJECT_NAME"
+source "$HOME/$PROJECT_NAME/bin/activate"
+pip install --no-index --upgrade pip
 
 # Setup the project and scratch directories
-PROJECT_NAME="retrieval-exploration"
-cd "$PROJECT/$USER"
+# NOTE: On some clusters (e.g. Narval), the PROJECT env var does not exist, so you will have to cd manually
+cd "$PROJECT/$USER" || exit
 git clone https://github.com/allenai/retrieval-exploration.git
-cd "retrieval-exploration"
+cd "retrieval-exploration" || exit
 mkdir -p "$SCRATCH/$PROJECT_NAME"
 
-# Create and activate the virtual environment
-poetry shell
-# poetry shell should activate the virtual environment, but it doesn't seem to? So do it manually:
-# See https://python-poetry.org/docs/basic-usage/#activating-the-virtual-environment for details.
-source $(poetry env info --path)/bin/activate
-
 # Install the package
-pip install --no-index --upgrade pip
-# This will be faster in most cases
-# See: https://github.com/python-poetry/poetry/issues/2094#issuecomment-605725577
-# poetry export -f requirements.txt --dev --extras "summarization" --without-hashes > requirements.txt
-# python -m pip install -r requirements.txt
-# poetry install
-poetry install --all-extras
-
-cd "$PROJECT/$USER/retrieval-exploration"
+# The package is built with poetry, so make sure that it is available first
+pip install "git+https://github.com/python-poetry/poetry.git"
+poetry -V
+# Unfortunately, poetry install causes a host of issues on ARC machines
+# As a workaround, install only the package with poetry, and all of its dependencies with pip
+poetry install --only-root
+# For several packages, there are less errors when installing the pre-built wheels
+pip install scipy numpy pandas torch torchvision --no-index
+# We also need the latest version of transformers, so install from git
+pip install "git+https://github.com/huggingface/transformers.git"
+# Lastly, we can export the project's dependencies to a requirements file and install with pip
+poetry export -f requirements.txt --output requirements.txt -E "summarization" --without-hashes
+# but first, remove any of the dependencies we are installing manually
+sed -i '/^scipy\|^numpy\|^pandas\|^torch\|^torchvision\|^transformers\|^pyarrow/d' requirements.txt 
+pip install -r requirements.txt
