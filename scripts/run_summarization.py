5abf216ba0e4560e4887af58c41e36e2e718275d
John Giorgi <johnmgiorgi@gmail.com>
2022-07-22 11:02:00 -0700

    Black format the codebase

--- scripts/run_summarization.py
+++ scripts/run_summarization.py
@@ -52,6 +52,7 @@ from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version, is_offline_mode, send_example_telemetry
 from transformers.utils.versions import require_version
 
+
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.21.0.dev0")
 
--- scripts/slurm/submit.sh
+++ scripts/slurm/submit.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Must be provided as argument to the script
 CONFIG_FILEPATH="$1"
+OUTPUT_DIR="$2"
 # Constants
 PERTURBATIONS=("backtranslation" "duplication" "addition" "deletion" "replacement")
 STRATEGIES=("random" "best-case" "worst-case")
 PERTURBED_FRAC=(0.1 0.5 1.0)
-PERTURBED_SEED=42
+
+# Run the baseline
+sbatch scripts/run.sh "$CONFIG_FILEPATH" "$OUTPUT_DIR/baseline"
 
 # Run the grid
 for perturbation in "${PERTURBATIONS[@]}";
@@ -13,17 +16,18 @@ do
     for strategy in "${STRATEGIES[@]}";
     do
         # Sorting does not need to run for multiple perturbed fractions
-        sbatch scripts/run_summarization.py "conf/base.yml" "$CONFIG_FILEPATH" \
-            perturbation="sorting" \
-            sampling_strategy="$strategy" \
-            perturbed_seed=$PERTURBED_SEED
+        sbatch scripts/run.sh "$CONFIG_FILEPATH" \
+            "$OUTPUT_DIR/perturbations/$strategy/sorting" \
+            "sorting" \
+            "$strategy"
 
         for perturbed_frac in "${PERTURBED_FRAC[@]}";
         do
-            sbatch scripts/run_summarization.py "conf/base.yml" "$CONFIG_FILEPATH" \
-                perturbation="${perturbation}" \
-                perturbed_frac="${perturbed_frac}" \
-                perturbed_seed=$PERTURBED_SEED
+            sbatch scripts/run.sh "$CONFIG_FILEPATH" \
+                "$OUTPUT_DIR/perturbations/$strategy/$perturbation/$perturbed_frac" \
+                "${perturbation}" \
+                "$strategy" \
+                "${perturbed_frac}"
         done
     done
 done
