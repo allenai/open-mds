b5b8ea936c3311e4ec3b4ecd90a887d95bdf33f4
John Giorgi <johnmgiorgi@gmail.com>
2022-07-26 14:21:44 -0700

    Basic sanitization of back-translated docs

--- src/retrieval_exploration/perturbations.py
+++ src/retrieval_exploration/perturbations.py
@@ -203,7 +203,7 @@ class Perturber:
                     # We backtranslate individual sentences, which improves backtranslation quality.
                     # This is likely because it more closely matches the MT models training data.
                     back_translated_sents = self._aug.augment(nltk.sent_tokenize(doc))
-                    back_translated_doc = " ".join(sent.strip() for sent in back_translated_sents)
+                    back_translated_doc = util.sanitize_text(" ".join(sent for sent in back_translated_sents))
                     reference[key] = back_translated_doc
                     back_translated_docs.append(back_translated_doc)
 
