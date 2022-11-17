604277fa6df433aed6b861eeae825d5cdd4987d4
johngiorgi <johnmgiorgi@gmail.com>
2022-11-17 12:07:49 -0500

    Document slurm scripts

--- scripts/slurm/perturb.sh
+++ scripts/slurm/perturb.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 # Requested resources
-#SBATCH --mem=36G
+#SBATCH --mem=32G
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:a100:1
 # Wall time and job details
-#SBATCH --time=3:00:00
+#SBATCH --time=5:00:00
 #SBATCH --job-name=perturb
 #SBATCH --account=def-wanglab-ab
-# Emails me when job starts, ends or fails
-#SBATCH --mail-user=johnmgiorgi@gmail.com
-#SBATCH --mail-type=FAIL
 # Use this command to run the same job interactively
-# salloc --mem=24G --cpus-per-task=1 --gres=gpu:a100:1 --time=3:00:00 --account=def-wanglab-ab
-# salloc --mem=24G --cpus-per-task=1 --gres=gpu:a100:1 --time=3:00:00 --account=def-gbader
+# salloc --mem=32G --cpus-per-task=1 --gres=gpu:a100:1 --time=3:00:00 --account=def-wanglab-ab
+# salloc --mem=32G --cpus-per-task=1 --gres=gpu:a100:1 --time=3:00:00 --account=def-gbader
 
 ### Example usage ###
-# sbatch "./scripts/slurm/perturb.sh" "./conf/multi_news/primera/eval.yml" \
-#   "./output/multi_news/random/addition/0.1" \
+# sbatch "./scripts/slurm/perturb.sh" "./conf/multinews/primera/eval.yml" \
+#   "./output/results/multinews/primera/peturbations/random/addition/0.1" \
 #   "addition" \
 #   "random" \
 #   "0.1"
 
-### Notes ###
-# PRIMERA on MultiNews requires <5hrs
-# PRIMERA on MultiXScience requires <3hrs
-# Pegasus on MultiNews requires <5hrs
-# LED on MS2 requires <3hrs
+### Usage notes ###
+# The amount of time needed will depend on the model and dataset, but it should be ~5 hours or less.
  
 ### Environment ###
 # Add your W&B key here to enable W&B reporting (or login with wandb login)
@@ -38,23 +32,17 @@ source "$HOME/$PROJECT_NAME/bin/activate"
 cd "$HOME/projects/def-gbader/$USER/$PROJECT_NAME" || exit
 
 ### Script arguments ###
-# Must be provided as argument to the script
-CONFIG_FILEPATH="$1"     # The path on disk to the yml config file
-OUTPUT_DIR="$2"          # The path on disk to save the output to
-PERTURBATION="$3"        # The perturbation to run
-SELECTION_STRATEGY="$4"  # The selection strategy to use for perturbed documents
-PERTURBED_FRAC="$5"      # The fraction of input documents to perturb
-# Allow these to be optional
-PERTURBATION=${PERTURBATION:=null}
-SELECTION_STRATEGY=${SELECTION_STRATEGY:=null}
-PERTURBED_FRAC=${PERTURBED_FRAC:=null}
-# Constants
-PERTURBED_SEED="42"
+# Required arguments
+CONFIG_FILEPATH="$1"           # The path on disk to the yml config file
+OUTPUT_DIR="$2"                # The path on disk to save the output to
+# Optional arguments
+PERTURBATION=${3:-null}        # The perturbation to run
+SELECTION_STRATEGY=${4:-null}  # The selection strategy to use for perturbed documents
+PERTURBED_FRAC=${5:-null}      # The fraction of input documents to perturb
 
 ### Job ###
 # This calls a modified version of the example summarization script from HF (with Trainer). For details,
 # see: https://github.com/huggingface/transformers/tree/main/examples/pytorch/summarization#with-trainer
-
 WANDB_MODE=offline \
 TRANSFORMERS_OFFLINE=1 \
 HF_DATASETS_OFFLINE=1 \
@@ -63,4 +51,6 @@ python "./scripts/run_summarization.py" "./conf/base.yml" "$CONFIG_FILEPATH" \
     perturbation="$PERTURBATION" \
     selection_strategy="$SELECTION_STRATEGY" \
     perturbed_frac="$PERTURBED_FRAC" \
-    perturbed_seed="$PERTURBED_SEED"
+    perturbed_seed="42"
+
+exit
--- scripts/slurm/submit_index.sh
+++ scripts/slurm/submit_index.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 ### Example usage ###
-# bash "./scripts/slurm/submit_index.sh" "multinews" "./output/datasets"
+# bash "./scripts/slurm/submit_index.sh" "multinews" "./output/datasets" 
+
+### Usage notes ###
+# If a dataset needs to be indexed, running this script for multiple strategies can lead to multiple jobs running
+# simultaneously, all trying to create the index and save it to disk at the same location, which is error-prone.
+# Therefore, it is advised to run the script once for a single retriever (say, "sparse") and a single strategy
+# (say, "mean"), allowing the index to be created, and then re-run the script for the remaining strategies.
+#
+# The larger datasets (e.g. those with 10s of thousands of example like multinews and ms2) can take around 12
+# hours to perform the index, retireval and rewriting steps. The smaller datasets (those with thousands or
+# hundreds of examples like wcep and cochrane) take considerably less time (e.g. < 3 hours).
 
 ### Script arguments ###
-# Must be provided as argument to the script
+# Required arguments
 HF_DATASET_NAME="$1"  # The name of a supported HuggingFace Dataset
 OUTPUT_DIR="$2"       # The path on disk to save the output to
-# Constants
-RETRIEVERS=("sparse")
-STRATEGIES=("mean" "max" "oracle")
-SPLITS="test"
+# Optional arguments
+RETRIEVERS=("sparse" "dense")
+STRATEGIES=("max" "mean" "oracle")
 
 ### Job ###
-
 # Run the grid
 for retriever in "${RETRIEVERS[@]}";
 do
@@ -22,7 +30,6 @@ do
         sbatch "./scripts/slurm/index.sh" "$HF_DATASET_NAME" \
             "$OUTPUT_DIR/${HF_DATASET_NAME}_${retriever}_${strategy}" \
             "${retriever}" \
-            "${strategy}" \
-            "$SPLITS"
+            "${strategy}"
     done
 done
